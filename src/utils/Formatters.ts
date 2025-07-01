41c9b47aad8e7e04fe3cd0bf2b05febed7fe5196
yurisantosdev <yuri.santos.cco@unochapeco.edu.br>
2025-07-01 15:19:45 -0300

    312312

--- src/utils/Formatters.ts
+++ src/utils/Formatters.ts
@@ -13,3 +13,14 @@ export function FormatarDataBrasileira(dataISO: any) {
 
   return dataFormatada.replace(',', '');
 }
+
+export function FormatarData(dataHora: string) {
+  const data = new Date(dataHora);
+
+  const dia = String(data.getDate()).padStart(2, '0');
+  const mes = String(data.getMonth() + 1).padStart(2, '0');
+  const hora = String(data.getHours()).padStart(2, '0');
+  const minutos = String(data.getMinutes()).padStart(2, '0');
+
+  return `${dia}/${mes} ${hora}:${minutos}`;
+}
--- src/utils/obterDataHoraAtual.ts
+++ src/utils/obterDataHoraAtual.ts
@@ -1,7 +1,9 @@
+import { FormatarData } from './Formatters';
+
 export function exibirDataHoraAtual() {
   const agora = new Date();
   const dataFormatada = agora.toLocaleDateString('pt-BR');
   const horaFormatada = agora.toLocaleTimeString('pt-BR');
 
-  return `${dataFormatada} ${horaFormatada}`;
-};
+  return FormatarData(`${dataFormatada} ${horaFormatada}`);
+}
